6378ff8d18fd20fcb38ce737b3e629f916d4a4d4
Leonardo <leonardoes2025@gmail.com>
2025-09-08 23:45:33 -0300

    adicionando schema de mensagem

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,25 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fastapi_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Olá mundo!'}
+    return Message(message='Olá mundo!')
+
+
+@app.get('/hello', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def hello():
+    return """
+    <html>
+        <title>Hello World!</title>
+        <body>
+            <h1>Hello Worldsss</h1>
+        </body>
+    </html>
+"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,21 @@ def test_root_deve_retornar_ola_mundo():
 
     assert response.json() == {'message': 'Olá mundo!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def hello_test():
+    client = TestClient(app)
+
+    response = client.get('/hello')
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        """
+    <html>
+        <title>Hello World!</title>
+        <body>
+            <h1>Hello Wor</h1>
+        </body>
+    </html>
+      """
+        in response.text
+    )
